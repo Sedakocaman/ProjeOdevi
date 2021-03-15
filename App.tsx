bc6af17c53f1f26e93bb1727d9a213f25caa27d5
Sedakocaman <45104282+Sedakocaman@users.noreply.github.com>
2021-03-15 23:04:15 +0300

    odev

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { useNavigation, NavigationContainer } from '@react-navigation/native';
+import Schedule from './Schedule';
+
+function  App() {
+  
+  return Schedule;
+
+}
+
+export default App();
---
+++ navigator.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+//import { createStackNavigator } from '@react-navigation/stack';
+import  QrCode from 'C:/Users/Seda/Desktop/reactOdevi/QrCode';
+import Schedule from '../Schedule';
+
+
+
+/*const AppStack = createStackNavigator();
+export default function Navigator(){
+
+    return (
+    <NavigationContainer>
+    <AppStack.Navigator screenOptions={{ headerShown: true }} >
+    <AppStack.Screen name="QrCode" component={QrCode} />
+    <AppStack.Screen name="Schedule" component={Schedule} />
+    </AppStack.Navigator>
+    </NavigationContainer>
+    );
+}*/
